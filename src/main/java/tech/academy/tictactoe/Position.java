800d59b015468869fcfaf35a0e4d99cc46e792b6
joanna.wrzolek <joanna.wrzolek@jeppesen.com>
2019-11-29 13:51:22 +0100

    Added project

---
+++ src/main/java/tech/academy/tictactoe/Position.java
@@ -0,0 +1,21 @@
+package tech.academy.tictactoe;
+
+public class Position {
+   private int x;
+    private int y;
+
+    public Position(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getPositionX(){
+        return x;
+    }
+
+    public int getPositionY(){
+        return y;
+    }
+
+
+}
